57f1dd875b0cb7eeb73802f1aefc8f65645be396
EDZ <edz@EDZdeMacBook-Pro-3.local>
2019-01-09 17:08:01 +0800

    vue3 test

---
+++ src/components/footer-component.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="footer-component">
+
+    </div>
+</template>
+<script>
+export default {
+    name:'footerComponent',
+    props:{
+
+    },
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+
+}
+</script>
+<style lang="less" scoped>
+    .footer-component{
+        width: 100%;
+        height: .98rem;
+        background: pink;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+    }
+</style>
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <footerComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import footerComponent from '@/components/footer-component.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    footerComponent
+  }
+}
+</script>
