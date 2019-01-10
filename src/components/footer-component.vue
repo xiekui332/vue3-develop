cd368e976e3f72a3822c572d56a303f595d9a092
EDZ <edz@EDZdeMacBook-Pro-3.local>
2019-01-10 16:40:00 +0800

    change  footer组件

--- src/components/footer-component.vue
+++ src/components/footer-component.vue
@@ -10,82 +10,91 @@
 </template>
 <script>
 export default {
-    name:'footerComponent',
-    props:{
-        obj:String    //设置类型
-    },
-    data(){
-        return{
-            footerItem:[
-                {
-                    img:this.obj == 'one'?require('../assets/images/icon/shyeselect.png'):require('../assets/images/icon/shyeunselect.png'),
-                    title:'首页',
-                    isActive:false
-                },
-                {
-                    img:this.obj == 'two'?require('../assets/images/icon/dashiselect.png'):require('../assets/images/icon/dashiunselect.png'),
-                    title:'大师讲堂',
-                    isActive:false
-                },
-                {
-                    img:this.obj == 'three'?require('../assets/images/icon/qifuselect.png'):require('../assets/images/icon/qifuunselect.png'),
-                    title:'祈福许愿',
-                    isActive:false
-                },
-                {
-                    img:this.obj == 'four'?require('../assets/images/icon/gerenselect.png'):require('../assets/images/icon/gerenunselect.png'),
-                    title:'个人中心',
-                    isActive:false
-                },
-            ]
+  name: "footerComponent",
+  props: {
+    obj: String //设置类型
+  },
+  data() {
+    return {
+      footerItem: [
+        {
+          img:
+            this.obj == "one"
+              ? require("../assets/images/icon/shyeselect.png")
+              : require("../assets/images/icon/shyeunselect.png"),
+          title: "首页",
+          isActive: false
+        },
+        {
+          img:
+            this.obj == "two"
+              ? require("../assets/images/icon/dashiselect.png")
+              : require("../assets/images/icon/dashiunselect.png"),
+          title: "大师讲堂",
+          isActive: false
+        },
+        {
+          img:
+            this.obj == "three"
+              ? require("../assets/images/icon/qifuselect.png")
+              : require("../assets/images/icon/qifuunselect.png"),
+          title: "祈福许愿",
+          isActive: false
+        },
+        {
+          img:
+            this.obj == "four"
+              ? require("../assets/images/icon/gerenselect.png")
+              : require("../assets/images/icon/gerenunselect.png"),
+          title: "个人中心",
+          isActive: false
         }
-    },
-    methods:{
-
-    },
-    created(){
-        // console.log(this.obj)
-    }
-
-}
+      ]
+    };
+  },
+  methods: {},
+  created() {
+    // console.log(this.obj)
+  }
+};
 </script>
 <style lang="less" scoped>
-    .footer-component{
-        width: 100%;
-        height: .98rem;
-        // background: pink;
-        background:rgba(250,250,250,0.9);
-        box-shadow:0px 1px 0px 0px rgba(0,0,0,0.25);
-        border-top: 1px solid #eeeeee;
-        box-sizing: border-box;
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        display: flex;
-        flex-direction: row;
+.footer-component {
+  width: 100%;
+  height: 0.98rem;
+  // background: pink;
+  background: rgba(250, 250, 250, 0.9);
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.25);
+  border-top: 1px solid #eeeeee;
+  box-sizing: border-box;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+}
+.footer_item {
+  flex: 1;
+  overflow: hidden;
+  img {
+    display: block;
+    width: 0.45rem;
+    height: 0.45rem;
+    margin: 0.1rem auto 0;
+  }
+  .footer_item_box {
+    display: flex;
+    flex-direction: column;
+    p {
+      color: #666;
+      font-size: 0.2rem;
+      margin-top: 0.05rem;
     }
-    .footer_item{
-        flex: 1;
-        overflow: hidden;
-        img{
-            display: block;
-            width: .45rem;
-            height: .45rem;
-            margin: 0.1rem auto 0;
-        };
-        .footer_item_box{
-            display: flex;
-            flex-direction: column;
-            p{
-                color: #666;
-                font-size: .2rem;
-                margin-top: 0.05rem;
-            };
-            p.p_active{
-                color: #D18E53;
-            }
-        }
+    p.p_active {
+      color: #d18e53;
     }
+  }
+}
 </style>
 
 
